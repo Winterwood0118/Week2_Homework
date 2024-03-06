5de27112578f7941abaa94814c4ce8f26fd82614
WinterWood0118 <dltjdals125@gmail.com>
2024-03-06 18:56:39 +0900

    나누기 0 예외처리

--- app/src/main/java/com/example/week2_homework/DivideOperation.kt
+++ app/src/main/java/com/example/week2_homework/DivideOperation.kt
@@ -2,6 +2,11 @@ package com.example.week2_homework
 
 class DivideOperation: Calculator() {
     init {
-        println("\nnum1 / num2 = ${num1 / num2}입니다.\n")
+        if(num2 == 0){
+            println("\n0으로는 나눌 수 없습니다.\n")
+        }
+        else{
+            println("\nnum1 / num2 = ${num1 / num2}입니다.\n")
+        }
     }
 }
