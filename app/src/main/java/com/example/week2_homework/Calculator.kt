3777c2e58fa083bb45ae6b8bbfbdeba04b05e514
WinterWood0118 <dltjdals125@gmail.com>
2024-03-06 17:28:19 +0900

    Level1 과제

--- app/src/main/java/com/example/week2_homework/Calculator.kt
+++ app/src/main/java/com/example/week2_homework/Calculator.kt
@@ -1,29 +1,51 @@
 package com.example.week2_homework
+// operator는 main에서 받아와서 사용
+class Calculator() {
+    var num1 = 0
+    var num2 = 0
+    // 인스턴스 생성과 동시에 num1, num2 입력
+    init {
+        //숫자를 입력할 때까지 반복
+        while (true) {
+            println("첫번째 수를 입력하세요.")
+            val input1 = readln()
 
-class Calculator(num1: Int, num2: Int, operator: String) {
-
-    fun calculator(num1:Int, num2:Int, operator:String) {
-
-
-    }
-}
-
-fun main(){
-
-
-    while(true) {
-        println("아래 연산자 중 사용할 연산자를 입력해주세요.")
-        println("1. 더하기: +")
-        println("2. 빼기: -")
-        println("3. 곱하기: *")
-        println("4. 나누기: /")
-        var operator = readLine()!!.toString()
-        when(operator){
-            "+", "-", "*", "/" -> break
-            else -> {
+            if(input1.all{ it.isDigit() }){
+                num1 = input1.toInt()
+                break
+            }else{
                 println("잘못된 입력입니다.")
             }
         }
-    }
+        //숫자를 입력할 때까지 반복
+        while (true) {
+            println("두번째 수를 입력하세요.")
+            val input2 = readln()
 
+            if(input2.all{ it.isDigit() }){
+                num2 = input2.toInt()
+                break
+            }else{
+                println("잘못된 입력입니다.")
+            }
+        }
+
+    }
+    //operator에 따라 사용할 함수 작성
+    fun addFun(){
+        val answer = num1 + num2
+        println("num1 + num2 = ${answer}입니다.")
+    }
+    fun subtractFun(){
+        val answer = num1 - num2
+        println("num1 - num2 = ${answer}입니다.")
+    }
+    fun multiplyFun(){
+        val answer = num1 * num2
+        println("num1 * num2 = ${answer}입니다.")
+    }
+    fun divideFun(){
+        val answer = num1 / num2
+        println("num1 / num2 = ${answer}입니다.")
+    }
 }
