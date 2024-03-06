5ad6a352197ef2813641d980fab5ec23625492f8
WinterWood0118 <dltjdals125@gmail.com>
2024-03-06 18:29:15 +0900

    Level2과제 만듦

--- app/src/main/java/com/example/week2_homework/Calculator.kt
+++ app/src/main/java/com/example/week2_homework/Calculator.kt
@@ -1,17 +1,34 @@
 package com.example.week2_homework
 // operator는 main에서 받아와서 사용
-class Calculator() {
+class Calculator(_operator: String){
+    //변수 선언
+    var operator = _operator
     var num1 = 0
     var num2 = 0
-    // 인스턴스 생성과 동시에 num1, num2 입력
+    //operator에 따라 사용할 함수 작성
+    fun addFun(){
+        println("\nnum1 + num2 = ${num1 + num2}입니다.\n")
+    }
+    fun subtractFun(){
+        println("\nnum1 - num2 = ${num1 - num2}입니다.\n")
+    }
+    fun multiplyFun(){
+        println("\nnum1 * num2 = ${num1 * num2}입니다.\n")
+    }
+    fun divideFun(){
+        println("\nnum1 / num2 = ${num1 / num2}입니다.\n")
+    }
+    fun moduloFun(){
+        println("\nnum1 % num2 = ${num1 % num2}입니다.\n")
+    }
     init {
-        //숫자를 입력할 때까지 반복
+            //숫자를 입력할 때까지 반복
         while (true) {
             println("첫번째 수를 입력하세요.")
-            val input1 = readln()
+            val input2 = readln()
 
-            if(input1.all{ it.isDigit() }){
-                num1 = input1.toInt()
+            if(input2.all{ it.isDigit() }){
+                num1 = input2.toInt()
                 break
             }else{
                 println("잘못된 입력입니다.")
@@ -20,32 +37,21 @@ class Calculator() {
         //숫자를 입력할 때까지 반복
         while (true) {
             println("두번째 수를 입력하세요.")
-            val input2 = readln()
-
-            if(input2.all{ it.isDigit() }){
-                num2 = input2.toInt()
+            val input3 = readln()
+            if(input3.all{ it.isDigit() }){
+                num2 = input3.toInt()
                 break
             }else{
                 println("잘못된 입력입니다.")
             }
         }
-
-    }
-    //operator에 따라 사용할 함수 작성
-    fun addFun(){
-        val answer = num1 + num2
-        println("num1 + num2 = ${answer}입니다.")
-    }
-    fun subtractFun(){
-        val answer = num1 - num2
-        println("num1 - num2 = ${answer}입니다.")
-    }
-    fun multiplyFun(){
-        val answer = num1 * num2
-        println("num1 * num2 = ${answer}입니다.")
-    }
-    fun divideFun(){
-        val answer = num1 / num2
-        println("num1 / num2 = ${answer}입니다.")
+        //Calculator 인스턴스를 생성해 클래스 내부의 init실행
+        when(operator){
+            "+" -> addFun()
+            "-" -> subtractFun()
+            "*" -> multiplyFun()
+            "/" -> divideFun()
+            "%" -> moduloFun()
+        }
     }
 }
--- app/src/main/java/com/example/week2_homework/CalculatorMain.kt
+++ app/src/main/java/com/example/week2_homework/CalculatorMain.kt
@@ -1,49 +1,48 @@
 package com.example.week2_homework
 
 fun main(){
-    println("이 프로그램은 두 정수 사이에서 간단한 사칙연산을 실행하는 계산기입니다.")
-    var operator: String = ""
+    println("이 프로그램은 두 수 사이에서 간단한 사칙연산을 실행하는 계산기입니다.")
+    var operator = ""
     while (true) {
-        println("아래 보기 중 사용할 번호를 입력하세요.")
-        println("1. 더하기")
-        println("2. 빼기")
-        println("3. 곱하기")
-        println("4. 나누기")
-        val input3 = readln()
-        when(input3){
-            "+", "-", "*", "/" -> {
-                operator = input3
-                break
-            }
+        println("아래 보기 중 사용할 기능의 번호를 입력하세요.")
+        println("1. 더하기 \t 2. 빼기 \t 3. 곱하기 \t 4. 나누기 \t 5. 나머지")
+        println("-1을 입력하면 계산기가 종료됩니다.")
+        val input1 = readln()
+        when (input1) {
             "1" -> {
+                println("더하기를 선택했습니다.")
                 operator = "+"
-                break
             }
+
             "2" -> {
+                println("빼기를 선택했습니다.")
                 operator = "-"
-                break
             }
+
             "3" -> {
+                println("곱하기를 선택했습니다.")
                 operator = "*"
-                break
             }
+
             "4" -> {
+                println("나누기를 선택했습니다.")
                 operator = "/"
+            }
+
+            "5" -> {
+                println("나머지를 선택했습니다.")
+                operator = "%"
+            }
+
+            "-1" -> {
+                println("계산기를 종료합니다.")
                 break
             }
             else -> {
                 println("잘못된 입력입니다.")
+                continue
             }
         }
+        var calc = Calculator(operator)
     }
-    //Calculator 인스턴스를 생성해 클래스 내부의 init실행
-    val calc = Calculator()
-
-    when(operator){
-        "+" -> calc.addFun()
-        "-" -> calc.subtractFun()
-        "*" -> calc.multiplyFun()
-        "/" -> calc.divideFun()
-    }
-
 }
