761a8e6e7f8d9070795ec332309a0943340c2816
WinterWood0118 <dltjdals125@gmail.com>
2024-03-06 17:38:05 +0900

    Level1 과제 수정, 클래스 내부에서 모두 처리하도록 변경

--- app/src/main/java/com/example/week2_homework/Calculator.kt
+++ app/src/main/java/com/example/week2_homework/Calculator.kt
@@ -1,36 +1,10 @@
 package com.example.week2_homework
 // operator는 main에서 받아와서 사용
 class Calculator() {
+    var operator: String = ""
     var num1 = 0
     var num2 = 0
-    // 인스턴스 생성과 동시에 num1, num2 입력
-    init {
-        //숫자를 입력할 때까지 반복
-        while (true) {
-            println("첫번째 수를 입력하세요.")
-            val input1 = readln()
 
-            if(input1.all{ it.isDigit() }){
-                num1 = input1.toInt()
-                break
-            }else{
-                println("잘못된 입력입니다.")
-            }
-        }
-        //숫자를 입력할 때까지 반복
-        while (true) {
-            println("두번째 수를 입력하세요.")
-            val input2 = readln()
-
-            if(input2.all{ it.isDigit() }){
-                num2 = input2.toInt()
-                break
-            }else{
-                println("잘못된 입력입니다.")
-            }
-        }
-
-    }
     //operator에 따라 사용할 함수 작성
     fun addFun(){
         val answer = num1 + num2
@@ -47,5 +21,65 @@ class Calculator() {
     fun divideFun(){
         val answer = num1 / num2
         println("num1 / num2 = ${answer}입니다.")
+    }
+
+    // 인스턴스 생성과 동시에 계산기 가동
+    init {
+        while (true) {
+            println("아래 보기 중 사용할 번호를 입력하세요.")
+            println("1. 더하기 \t 2. 빼기 \t 3. 곱하기 \t 4. 나누기")
+            val input1 = readln()
+            when(input1){
+                "1" -> {
+                    operator = "+"
+                    break
+                }
+                "2" -> {
+                    operator = "-"
+                    break
+                }
+                "3" -> {
+                    operator = "*"
+                    break
+                }
+                "4" -> {
+                    operator = "/"
+                    break
+                }
+                else -> {
+                    println("올바른 번호를 입력하세요.")
+                }
+            }
+        }
+        //숫자를 입력할 때까지 반복
+        while (true) {
+            println("첫번째 수를 입력하세요.")
+            val input2 = readln()
+
+            if(input2.all{ it.isDigit() }){
+                num1 = input2.toInt()
+                break
+            }else{
+                println("잘못된 입력입니다.")
+            }
+        }
+        //숫자를 입력할 때까지 반복
+        while (true) {
+            println("두번째 수를 입력하세요.")
+            val input3 = readln()
+
+            if(input3.all{ it.isDigit() }){
+                num2 = input3.toInt()
+                break
+            }else{
+                println("잘못된 입력입니다.")
+            }
+        }
+        when(operator){
+            "+" -> addFun()
+            "-" -> subtractFun()
+            "*" -> multiplyFun()
+            "/" -> divideFun()
+        }
     }
 }
--- app/src/main/java/com/example/week2_homework/CalculatorMain.kt
+++ app/src/main/java/com/example/week2_homework/CalculatorMain.kt
@@ -2,48 +2,7 @@ package com.example.week2_homework
 
 fun main(){
     println("이 프로그램은 두 정수 사이에서 간단한 사칙연산을 실행하는 계산기입니다.")
-    var operator: String = ""
-    while (true) {
-        println("아래 보기 중 사용할 번호를 입력하세요.")
-        println("1. 더하기")
-        println("2. 빼기")
-        println("3. 곱하기")
-        println("4. 나누기")
-        val input3 = readln()
-        when(input3){
-            "+", "-", "*", "/" -> {
-                operator = input3
-                break
-            }
-            "1" -> {
-                operator = "+"
-                break
-            }
-            "2" -> {
-                operator = "-"
-                break
-            }
-            "3" -> {
-                operator = "*"
-                break
-            }
-            "4" -> {
-                operator = "/"
-                break
-            }
-            else -> {
-                println("잘못된 입력입니다.")
-            }
-        }
-    }
     //Calculator 인스턴스를 생성해 클래스 내부의 init실행
     val calc = Calculator()
-
-    when(operator){
-        "+" -> calc.addFun()
-        "-" -> calc.subtractFun()
-        "*" -> calc.multiplyFun()
-        "/" -> calc.divideFun()
-    }
 
 }
