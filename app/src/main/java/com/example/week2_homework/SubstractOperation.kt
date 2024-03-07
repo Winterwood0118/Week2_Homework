9421249604c9d16a8c706ea6ab7e2a730decc1d0
WinterWood0118 <dltjdals125@gmail.com>
2024-03-07 17:11:40 +0900

    lv4 v0.1.3 실수 연산 기능 추가

--- app/src/main/java/com/example/week2_homework/SubstractOperation.kt
+++ app/src/main/java/com/example/week2_homework/SubstractOperation.kt
@@ -1,7 +1,13 @@
 package com.example.week2_homework
 
+import kotlin.math.round
+
 class SubstractOperation : AbstractOperation() {
     override fun operate(num1: Int, num2: Int) {
         println("\n$num1 - $num2 = ${num1 - num2}입니다.\n")
+    }
+
+    override fun operate(num1: Double, num2: Double) {
+        println("\n$num1 - $num2 = ${round((num1 - num2) * 100.0) / 100.0}입니다.\n")
     }
 }
