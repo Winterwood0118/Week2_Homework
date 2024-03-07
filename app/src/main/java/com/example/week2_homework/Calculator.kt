e81c939a74e257297da2d970c545b01028f7278a
WinterWood0118 <dltjdals125@gmail.com>
2024-03-07 17:20:17 +0900

    lv4 v0.1.4_1 변수 수정

--- app/src/main/java/com/example/week2_homework/Calculator.kt
+++ app/src/main/java/com/example/week2_homework/Calculator.kt
@@ -1,7 +1,7 @@
 package com.example.week2_homework
 
 // 출력함수
-class Calculator(val operation: AbstractOperation) {
+class Calculator(private val operation: AbstractOperation) {
     fun result(num1: Int, num2: Int) {
         operation.operate(num1, num2)
     }
